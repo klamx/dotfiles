0f88edd375bf8e9950c4ae90ce3d4262867d188b
Alejandro Largo <largoalejo@gmail.com>
2024-03-10 22:39:55 -0500

    refactor: modularizacion de los pluggins

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = { "c",
+        "cpp", 
+        "lua", 
+        "vim", 
+        "vimdoc", 
+        "query", 
+        "javascript",
+        "html" },
+      -- sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },  
+    })
+  end
+}
